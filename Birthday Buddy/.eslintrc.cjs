162df6227ef89a734c8c370c8f5affa8a282d518
Billy Torres <btorresr75@gmail.com>
2023-08-15 17:05:49 -0600

    Resolviendo conflictos entre Eslint y Prettier

--- Birthday Buddy/.eslintrc.cjs
+++ Birthday Buddy/.eslintrc.cjs
@@ -1,21 +1,29 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-    "prettier"
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ["standard", "plugin:react/recommended", "prettier"],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [".eslintrc.{js,cjs,jsx}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react"],
+	rules: {
+		"react/react-in-jsx-scope": "off",
+		quotes: ["error", "double"],
+		"space-before-function-paren": "off",
+		"react/prop-types": 0,
+	},
+};
--- README.md
+++
@@ -1,7 +0,0 @@
-# Practicas-con-react
-Repositorio para reforzar, aprender y practicar conceptos aprendidos en react. Por medio de replicas o clones de projectos practicar la sintaxis y estructura de ReactJS
-
-# Informacion <br/>
---Autor: https://github.com/john-smilga <br/>
---Repositorio: https://github.com/john-smilga/react-projects
-
