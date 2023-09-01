2111e2f9d830eb56143bfdb707ce5b7af0ecd91f
Billy Torres <btorresr75@gmail.com>
2023-09-01 17:33:18 -0600

    Finalizado de estilos en projecto

--- Tours/src/components/Tours.jsx
+++ Tours/src/components/Tours.jsx
@@ -2,7 +2,7 @@ import Tour from "./Tour";
 
 const Tours = () => {
 	return (
-		<div className="w-[70%] py-[2rem] mx-auto">
+		<div className="w-[70%] py-[2rem] mx-auto hover:shadow-md">
 			<div className="mb-8">
 				<h2 className="text-center text-[3.5rem]">Our Tours</h2>
 				<div className="w-[100px] h-[6px] bg-greenColor rounded-[10px] my-0 mx-auto"></div>
