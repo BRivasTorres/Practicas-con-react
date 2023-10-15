508bacfdfa50542cec46c3fe8b331035a5479d16
Billy Torres <btorresr75@gmail.com>
2023-10-15 15:17:07 -0600

    styles : card info styles

--- Tabs/src/components/DataCard.jsx
+++ Tabs/src/components/DataCard.jsx
@@ -4,29 +4,28 @@ import { faAnglesRight } from "@fortawesome/free-solid-svg-icons";
 
 const DataCard = () => {
 	return (
-		<div>
-			{data.map((card, id) => {
+		<section className="flex flex-col gap-4">
+			<div className="flex flex-col mb-6">
+				<h2 className="text-[2.3rem]">{data[0].role}</h2>
+				<span className="uppercase bg-[#CBD5E1] p-[.5rem] w-fit rounded ">
+					{data[0].name}{" "}
+				</span>
+				<small className="capitalize text-[1rem]">{data[0].date}</small>
+			</div>
+			{data[0].description.map((desc, id) => {
 				return (
-					<section key={id} className="">
-						<div>
-							<h2>{card.role}</h2>
-							<h3>{card.name} </h3>
-							<small>{card.date}</small>
-						</div>
-						<div className="flex flex-col ">
-							{card.description.map((cardDesc, id) => {
-								return (
-									<i key={id} className="">
-										<FontAwesomeIcon icon={faAnglesRight} />
-										{cardDesc}
-									</i>
-								);
-							})}
-						</div>
-					</section>
+					<div key={id} className="flex items-center gap-7">
+						<i>
+							<FontAwesomeIcon
+								icon={faAnglesRight}
+								className="text-[#14BEC4]"
+							/>
+						</i>
+						<p>{desc}</p>
+					</div>
 				);
 			})}
-		</div>
+		</section>
 	);
 };
 
