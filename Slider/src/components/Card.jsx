cebc3c618717d016ecfed0a612f10dc55fe3f563
Billy Torres <btorresr75@gmail.com>
2023-10-20 17:49:03 -0600

    feat : añadiendo botones

--- Slider/src/components/Card.jsx
+++ Slider/src/components/Card.jsx
@@ -1,39 +1,41 @@
-import data from "../mocks/data";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faSquareCaretRight,
 	faSquareCaretLeft,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Card = () => {
+const Card = ({ handleClick, currItem }) => {
 	return (
 		<div className="flex items-center">
-			<button className="text-[2.5rem] p-[1rem] text-[#64748B] hover:text-[#8B5CF6]">
-				<FontAwesomeIcon icon={faSquareCaretLeft} className="" />
+			<button
+				className="text-[2.5rem] p-[1rem] text-[#64748B] hover:text-[#8B5CF6]"
+				onClick={() => handleClick("prev")}
+			>
+				<FontAwesomeIcon icon={faSquareCaretLeft} />
 			</button>
 			<article className="w-[90%] mx-auto flex flex-col items-center p-[1.5rem]">
 				<div className="flex flex-col items-center mb-[1.35rem] ">
 					<img
-						src={data[0].image}
-						alt={data[0].name}
+						src={currItem.image}
+						alt={currItem.name}
 						className="w-[160px] h-[160px] rounded-[100%] mb-[1rem] shadow-img "
 					/>
 					<h2 className="text-[#8B5CF6] uppercase text-[1.5rem] ">
-						{data[0].name}
+						{currItem.name}
 					</h2>
 					<span className="text-[#334155] capitalize text-[1.2rem]">
-						{data[0].title}{" "}
+						{currItem.title}{" "}
 					</span>
 				</div>
-				<p className="text-center text-[#334155] capitalize text-[1.2rem] ">
-					{data[0].quote}{" "}
+				<p className="text-center text-[#334155] capitalize text-[1.2rem]">
+					{currItem.quote}{" "}
 				</p>
 			</article>
-			<button className="hover:text-[#8B5CF6]">
-				<FontAwesomeIcon
-					icon={faSquareCaretRight}
-					className="text-[2.5rem] p-[1rem] text-[#64748B]"
-				/>
+			<button
+				className="text-[2.5rem] p-[1rem] text-[#64748B] hover:text-[#8B5CF6]"
+				onClick={() => handleClick("next")}
+			>
+				<FontAwesomeIcon icon={faSquareCaretRight} />
 			</button>
 		</div>
 	);
--- Slider/src/components/Slider.jsx
+++ Slider/src/components/Slider.jsx
@@ -1,9 +1,29 @@
 import Card from "./Card";
+import { useState } from "react";
+import data from "../mocks/data";
 
 const Slider = () => {
+	const [currItem, setCurrItem] = useState(data[0]);
+
+	const handleClick = (btn) => {
+		let currIndex = data.indexOf(currItem);
+
+		if (currIndex < 0) {
+			setCurrItem(data[data.length - 1]);
+		} else if (currIndex > data.length - 1) {
+			setCurrItem(data[0]);
+		}
+
+		if (btn === "prev") {
+			setCurrItem(data[currIndex - 1]);
+		} else if (btn === "next") {
+			setCurrItem(data[currIndex + 1]);
+		}
+	};
+
 	return (
-		<div className="w-[60%] ">
-			<Card />{" "}
+		<div className="w-[60%]">
+			<Card handleClick={handleClick} currItem={currItem} />{" "}
 		</div>
 	);
 };
