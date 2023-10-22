1fc34d2502a43a00a4ddb79ccb78cee711b871e5
Billy Torres <btorresr75@gmail.com>
2023-10-22 09:28:15 -0600

    Proyecto Lorem Ipsum -- 7 finalizado

--- Lorem Ipsum/src/App.jsx
+++ Lorem Ipsum/src/App.jsx
@@ -1,9 +1,10 @@
+import LoremIpsum from "./components/LoremIpsum";
+
 function App() {
 	return (
-		<>
-			{" "}
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</>
+		<div className="flex justify-center">
+			<LoremIpsum />
+		</div>
 	);
 }
 
