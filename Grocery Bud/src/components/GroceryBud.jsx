3c8b70fc1841e8c629535199187fe7752c7f4c4c
Billy Torres <btorresr75@gmail.com>
2023-11-03 17:30:57 -0600

    feat : funcionalidad para agregar items

--- Grocery Bud/src/components/GroceryBud.jsx
+++ Grocery Bud/src/components/GroceryBud.jsx
@@ -1,19 +1,41 @@
 import Task from "./Task";
+import { useState } from "react";
+import { v4 as id } from "uuid";
 
 const GroceryBud = () => {
+	const [input, setInput] = useState("");
+	const [tasks, setTasks] = useState([]);
+
+	const handleInput = (e) => {
+		setInput(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (input.trim() !== "") {
+			setTasks([...tasks, { id: id(), value: input }]);
+			setInput("");
+		}
+	};
+
 	return (
 		<div className="w-[40%] mx-auto my-[4rem] bg-[#F8FAFC] max-w-[800px] shadow-sm flex flex-col items-center p-[3rem] rounded-md hover:shadow-lg transition-all duration-500 ease-linear">
 			<h2 className="text-[2rem] mb-[2rem] ">Grocery Bud</h2>
-			<form className="mb-[2rem] min-w-[300px] w-[100%] form ">
+			<form
+				className="mb-[2rem] min-w-[300px] w-[100%] form"
+				onSubmit={handleSubmit}
+			>
 				<input
 					type="text"
+					value={input}
+					onChange={handleInput}
 					className="bg-[#FFFFFF] p-[.5rem]  border-[2px] border-gray-200"
 				/>
 				<button className="bg-[#06B6D4] p-[.5rem] rounded-r-lg text-white">
 					Add Item
 				</button>
 			</form>
-			<Task />
+			<Task tasks={tasks} />
 		</div>
 	);
 };
--- Grocery Bud/src/components/Task.jsx
+++ Grocery Bud/src/components/Task.jsx
@@ -1,11 +1,18 @@
-const Task = () => {
+const Task = ({ tasks }) => {
 	return (
-		<div className="task w-[100%]">
-			<input type="checkbox" name="check" id="check" />
-			<span className="text-[1.3rem] ">task</span>
-			<button className="bg-black text-white p-[.2rem] rounded-sm">
-				Delete
-			</button>
+		<div>
+			{tasks &&
+				tasks.map((item) => {
+					return (
+						<div key={item.id} className="task w-[100%] mb-[1rem] ">
+							<input type="checkbox" name="check" id="check" />
+							<span className="text-[1rem]">{item.value}</span>
+							<button className="bg-black text-white p-[.2rem] rounded-sm">
+								Delete
+							</button>
+						</div>
+					);
+				})}
 		</div>
 	);
 };
