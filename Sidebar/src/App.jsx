67ad82310fbe36d14726a00237a34b0e343e10b0
Billy Torres <btorresr75@gmail.com>
2023-11-19 17:40:49 -0600

    feat : React router y creacion de paginas

--- Sidebar/src/App.jsx
+++ Sidebar/src/App.jsx
@@ -1,13 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Team from "./pages/Team";
+import Documents from "./pages/Team";
+import Calendar from "./pages/Team";
 
 function App() {
 	return (
 		<div className="bg-[#F1F5F9] h-[100dvh] ">
-			<Sidebar />
 			<BrowserRouter>
+				<Sidebar />
 				<Routes>
-					<Route />
+					<Route path="/home" element={Home} />
+					<Route path="/team" element={Team} />
+					<Route path="/projects" element={Projects} />
+					<Route path="/documents" element={Documents} />
+					<Route path="/calendar" element={Calendar} />
 				</Routes>
 			</BrowserRouter>
 		</div>
--- Sidebar/src/components/Menu.jsx
+++ Sidebar/src/components/Menu.jsx
@@ -1,5 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
+
+//TODO hacer el menu tabs hidden, y el menu navegable visible cuando este sea precionado.
 
 const Menu = () => {
 	return (
@@ -10,6 +13,13 @@ const Menu = () => {
 					className="text-[1.5rem] text-[#49A6E9] menu-animation hover:cursor-pointer"
 				/>
 			</div>
+			<nav>
+				<NavLink to={"/home"}>Home</NavLink>
+				<NavLink to={"/documents"}>Documents</NavLink>
+				<NavLink to={"/team"}>Team</NavLink>
+				<NavLink to={"/projects"}>Projects</NavLink>
+				<NavLink to={"/calendar"}>Calendar</NavLink>
+			</nav>
 		</div>
 	);
 };
