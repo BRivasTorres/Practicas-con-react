e9dd941f80926516afb73356c270b2947c851e65
Billy Torres <btorresr75@gmail.com>
2023-11-19 09:58:22 -0600

    proyecto 11 Navbar finalizado

--- Navbar/src/App.jsx
+++ Navbar/src/App.jsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+
 function App() {
 	return (
-		<>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</>
+		<div className="bg-[#F8FAFC] h-[100dvh] ">
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/home" element={<Home />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/profile" element={<Profile />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
 	);
 }
 
