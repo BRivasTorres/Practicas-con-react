96a440e1eec98163013c460acb493b78a115a731
Billy Torres <btorresr75@gmail.com>
2023-12-17 11:45:39 -0600

    proyecto card 14

---
+++ Cart/src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+  return (
+		<header className="bg-[#645CFF]">
+            <div className="w-[50%] mx-auto flex justify-between items-center p-[2rem] text-white text-[2rem] ">
+                <h2>UseReducer</h2>
+                <FontAwesomeIcon icon={faCartPlus} />
+            </div>
+		</header>
+  );
+}
+
+export default Header
