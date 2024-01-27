df359e8d9cc2bd98e92134a70f2f56d7836c9838
Billy Torres <btorresr75@gmail.com>
2024-01-27 08:09:32 -0600

    feat : fetch data of single cocktail

--- MixMaster/src/components/SingleCocktail.jsx
+++ MixMaster/src/components/SingleCocktail.jsx
@@ -1,10 +1,47 @@
-//TODO renderizar los datos recibidos del componente padre
+import { Link } from "react-router-dom";
+
 const SingleCocktail = ({data}) => {
+  
+  const {name, image, category, glass, ingredients, instructions, info } = data
+  
   return (
-    <div>
-      <h2>{data.name}</h2>
-    </div>
-  )
+		<section className="border border-red-700 w-[50%] mx-auto pt-[4rem] flex flex-col items-center ">
+			<Link to="/" className="bg-greenCustom text-white text-[1.2rem] capitalize px-[.5rem] py-[.2rem] rounded-[.4rem] w-fit ">
+				back home
+			</Link>
+			<h2 className="text-[2rem] font-semibold ">{name}</h2>
+			<div className="grid grid-cols-[_1fr_1fr] mt-[5rem] items-center ">
+				<img src={image} alt={name} className="h-[400px] rounded-lg "></img>
+				<div>
+					<p>
+						<span>name :</span> {name}
+					</p>
+					<p>
+						<span>category :</span>{" "}
+						{category}
+					</p>
+					<p>
+						<span>info :</span> {info}
+					</p>
+					<p>
+						<span>glass :</span> {glass}
+					</p>
+					<p>
+						<span>instructons :</span>{" "}
+						{instructions}
+					</p>
+					<p>
+						<span>ingredients :</span>
+						{ingredients.map((item, index) => {
+							return item ? (
+								<span key={index}> {item}</span>
+							) : null;
+						})}
+					</p>
+				</div>
+			</div>
+		</section>
+  );
 }
 
 export default SingleCocktail
--- MixMaster/src/pages/Cocktail.jsx
+++ MixMaster/src/pages/Cocktail.jsx
@@ -6,63 +6,71 @@ import SingleCocktail from "../components/SingleCocktail";
 
 const Cocktail = () => {
     const { id } = useParams();
-    const [cocktail, setCocktail] = useState()
+    const [cocktail, setCocktail] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     
     useEffect(() => {
+        setIsLoading(true)
         const fetchData = async () => {
-			const response = await getData(
-				`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
-			);
-            setCocktail(response)
-            setIsLoading(false);
-		};
+            try {
+				const data = await getData(
+					`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+				);
+				if (data.drinks) {
+					const {
+						strDrink: name,
+						strDrinkThumb: image,
+						strAlcoholic: info,
+						strCategory: category,
+						strGlass: glass,
+						strInstructions: instructions,
+						strIngredient1,
+						strIngredient2,
+						strIngredient3,
+						strIngredient4,
+						strIngredient5,
+					} = data.drinks[0];
+					const ingredients = [
+						strIngredient1,
+						strIngredient2,
+						strIngredient3,
+						strIngredient4,
+						strIngredient5,
+					];
+					const newCocktail = {
+						name,
+						image,
+						info,
+						category,
+						glass,
+						instructions,
+						ingredients,
+					};
+					setCocktail(newCocktail);
+				} else {
+					setCocktail(null);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+            setIsLoading(false)
+        }
         fetchData()
     }, [id])
     
     if(isLoading) {
-        <Loading />
-    } else {
-        const {
-            strDrink: name,
-            strDrinkThumb: image,
-            strAlcoholic: info,
-            strCategory: category,
-            strGlass: glass,
-            strInstructions: instructions,
-            strIngredient1,
-            strIngredient2,
-            strIngredient3,
-            strIngredient4,
-            strIngredient5,
-        } = cocktail.drinks[0];      
-        const ingredients = [
-            strIngredient1,
-            strIngredient2,
-            strIngredient3,
-            strIngredient4,
-            strIngredient5,
-        ];
-        const newCocktail = {
-            name,
-            image,
-            info,
-            category,
-            glass,
-            instructions,
-            ingredients,
-        };
-        setCocktail(newCocktail)
-        
+        return <Loading />
+    } 
+    
+    if(!cocktail) {
+        <h2>No cocktail to display</h2>
+    } else {        
         return (
-            <div>
-                {/* <SingleCocktail data={cocktail} /> */}
-                {console.log(cocktail.drinks)}
-            </div>
-        );
-    }
-    
-    
+			<div className="bg-[#F8FAFC] min-h-[100vh] ">
+				<SingleCocktail data={cocktail} />
+			</div>
+		);
+    } 
 }
 
 export default Cocktail
