0f8095c111726d149f54acb4fdfa019ba890c8f9
Billy Torres <btorresr75@gmail.com>
2024-01-27 11:35:57 -0600

    Finish Proyect MixMaster

--- MixMaster/src/App.jsx
+++ MixMaster/src/App.jsx
@@ -5,21 +5,41 @@ import Newsletter from "./pages/Newsletter";
 import Home from "./pages/Home";
 import Cocktail from "./pages/Cocktail";
 import { DataProvider } from "./context/DataContext";
+import { ToastContainer } from "react-toastify";
+
 
 function App() {
   return (
 		<>
-		<DataProvider>
-			<BrowserRouter>
-				<Header />
-				<Routes>
-					<Route path="/" element={<Home />} ></Route>
-					<Route path="/about" element={<About />} ></Route>
-					<Route path="/newsletter" element={<Newsletter />} ></Route>
-					<Route path="/cocktail/:id" element={<Cocktail />} ></Route>
-				</Routes>
-			</BrowserRouter>
-		</DataProvider>
+			<DataProvider>
+				<BrowserRouter>
+					<Header />
+					<Routes>
+						<Route path="/" element={<Home />}></Route>
+						<Route path="/about" element={<About />}></Route>
+						<Route
+							path="/newsletter"
+							element={<Newsletter />}
+						></Route>
+						<Route
+							path="/cocktail/:id"
+							element={<Cocktail />}
+						></Route>
+					</Routes>
+				</BrowserRouter>
+				<ToastContainer
+					position="top-right"
+					autoClose={5000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+					theme="light"
+				/>
+			</DataProvider>
 		</>
   );
 }
