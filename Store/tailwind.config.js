5f8daac287b8586614823f9c689688ce4c47bc0c
Billy Torres <btorresr75@gmail.com>
2024-04-01 17:31:10 -0700

    feat : Init proyect Store

---
+++ Store/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"nav-bg-light": "#F0F6FF",
+				"main-bg-light": "#FFFFFF",
+				"text-light": "#394E6A",
+				"bg-products-light": "#C149AD",
+				"active-bg-light": "#021431",
+				"btns-bg-dard": "#FF7AC6",
+				"text-dark": "#F8F8F2",
+				"active-bg-dark": "#414558",
+				"bg-products-dark": "#FFA94D",
+			},
+		},
+	},
+	plugins: [],
+};
+
