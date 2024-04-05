63baeb38abd1f3ea46bd541fd4a2977cff770406
Billy Torres <btorresr75@gmail.com>
2024-04-04 20:26:23 -0700

    fix : Fix active page styles

--- Store/src/components/header/Nav.jsx
+++ Store/src/components/header/Nav.jsx
@@ -1,29 +1,66 @@
 import { NavLink } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
 
 const Nav = () => {
   return (
-		<nav className="bg-nav-bg-light text-text-light  ">
-			<NavLink
-				to={"/"}
-				// className="hover:bg-nav-hov-light p-[1rem] "
-				className={({ isActive }) =>
-					isActive ? "bg-nav-bg-active" : "bg-red-700"
-				}
+		<nav className="bg-bg-light text-text-light py-[1.5rem] flex items-center justify-around border">
+			<a
+				href="/"
+				className="bg-[#057AFF] w-[50px] h-[50px] grid place-content-center font-semibold text-[2rem] text-white leading-[2.25rem] rounded-[8px] "
 			>
-				Home
-			</NavLink>
-			<NavLink
-				to={"/about"}
-				// className="hover:bg-nav-hov-light"
-			>
-				About
-			</NavLink>
-			<NavLink to={"/products"} className="hover:bg-nav-hov-light">
-				Products
-			</NavLink>
-			<NavLink to={"/cart"} className="hover:bg-nav-hov-light">
-				Cart
-			</NavLink>
+				C
+			</a>
+			<ul>
+				<NavLink
+					to={"/"}
+					className={({ isActive }) =>
+						isActive
+							? "bg-active-bg-light text-nav-hov-text px-[1rem] py-[.5rem] rounded-[7px] "
+							: "nav-link"
+					}
+				>
+					Home
+				</NavLink>
+				<NavLink
+					to={"/about"}
+					className={({ isActive }) =>
+						isActive
+							? "bg-active-bg-light text-nav-hov-text px-[1rem] py-[.5rem] rounded-[7px]"
+							: "nav-link"
+					}
+				>
+					About
+				</NavLink>
+				<NavLink
+					to={"/products"}
+					className={({ isActive }) =>
+						isActive
+							? "bg-active-bg-light text-nav-hov-text px-[1rem] py-[.5rem] rounded-[7px]"
+							: "nav-link "
+					}
+				>
+					Products
+				</NavLink>
+				<NavLink
+					to={"/cart"}
+					className={({ isActive }) =>
+						isActive
+							? "bg-active-bg-light text-nav-hov-text px-[1rem] py-[.5rem] rounded-[7px]"
+							: "nav-link"
+					}
+				>
+					Cart
+				</NavLink>
+			</ul>
+			<section>
+				<div>
+					<FontAwesomeIcon icon={faSun} />
+					<FontAwesomeIcon icon={faMoon} />
+				</div>
+				<FontAwesomeIcon icon={faCartShopping} />
+			</section>
 		</nav>
   );
 }
--- Store/tailwind.config.js
+++ Store/tailwind.config.js
@@ -4,14 +4,15 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				"nav-bg-light": "#F0F6FF",
-				"nav-bg-active": "#021431", 
-				"nav-hov-light": "#EAF0FA",
-				"main-bg-light": "#FFFFFF",
+				"bg-light": "#F0F6FF",
 				"text-light": "#394E6A",
+				"nav-hov-light": "#EAF0FA",
+				"nav-hov-text": "#C7C9D1",
+				"main-bg-light": "#FFFFFF",
 				"bg-products-light": "#C149AD",
 				"active-bg-light": "#021431",
-				"btns-bg-dard": "#FF7AC6",
+				
+				"btns-bg-dark": "#FF7AC6",
 				"text-dark": "#F8F8F2",
 				"active-bg-dark": "#414558",
 				"bg-products-dark": "#FFA94D",
