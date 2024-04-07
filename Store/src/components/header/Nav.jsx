6fdd3e6c2c188100d13e7d6c13223f64581c981c
Billy Torres <btorresr75@gmail.com>
2024-04-07 11:56:50 -0700

    feat : Finish component Nav

--- Store/src/components/header/Nav.jsx
+++ Store/src/components/header/Nav.jsx
@@ -3,78 +3,73 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import ThemeContext from "../../context/ThemeContext";
 import { useContext } from "react";
+import NavLinkComponent from "./NavLinkComponent";
+import navigationPages from "../../mocks/NavigationPages";
 
 const Nav = () => {
 	const {theme, handleChangeTheme} = useContext(ThemeContext);
 	
   return (
 		<nav
-			className={` bg-bg-light text-text-light py-[1.5rem] flex items-center justify-around`}
+			className={`${
+				theme === "dark"
+					? "bg-bg-dark text-text-dark"
+					: "bg-bg-light text-text-light"
+			}  py-[1.5rem] flex items-center justify-around`}
 		>
 			<NavLink
 				to="/"
-				className="bg-[#057AFF] w-[50px] h-[50px] grid place-content-center font-semibold text-[2rem] text-white leading-[2.25rem] rounded-[8px]"
+				className={`${
+					theme === "dark"
+						? "bg-btns-bg-dark text-bg-dark"
+						: "bg-[#057AFF] text-white "
+				}  w-[50px] h-[50px] grid place-content-center font-semibold text-[2rem]  leading-[2.25rem] rounded-[8px]`}
 			>
 				C
 			</NavLink>
-			<ul>
-				<NavLink
-					to={"/"}
-					className={({ isActive }) =>
-						isActive
-							? "bg-active-bg-light text-nav-hov-text px-[1rem] py-[.5rem] rounded-[7px] "
-							: "nav-link"
-					}
-				>
-					Home
-				</NavLink>
-				<NavLink
-					to={"/about"}
-					className={({ isActive }) =>
-						isActive
-							? "bg-active-bg-light text-nav-hov-text px-[1rem] py-[.5rem] rounded-[7px]"
-							: "nav-link"
-					}
-				>
-					About
-				</NavLink>
-				<NavLink
-					to={"/products"}
-					className={({ isActive }) =>
-						isActive
-							? "bg-active-bg-light text-nav-hov-text px-[1rem] py-[.5rem] rounded-[7px]"
-							: "nav-link "
-					}
-				>
-					Products
-				</NavLink>
-				<NavLink
-					to={"/cart"}
-					className={({ isActive }) =>
-						isActive
-							? "bg-active-bg-light text-nav-hov-text px-[1rem] py-[.5rem] rounded-[7px]"
-							: "nav-link"
-					}
-				>
-					Cart
-				</NavLink>
+			<ul className="flex">
+				{navigationPages.map((navItem) => {
+					return (
+						<NavLinkComponent
+							key={navItem.id}
+							link={navItem.to}
+							label={navItem.label}
+						/>
+					);
+				})}
 			</ul>
 			<section className="flex gap-x-[1rem] text-[1.3rem] ">
 				<div>
 					{theme === "dark" ? (
 						<button onClick={() => handleChangeTheme("light")}>
-							<FontAwesomeIcon icon={faMoon} />
+							<FontAwesomeIcon icon={faSun} />
 						</button>
 					) : (
 						<button onClick={() => handleChangeTheme("dark")}>
-							<FontAwesomeIcon icon={faSun} />
+							<FontAwesomeIcon icon={faMoon} />
 						</button>
 					)}
 				</div>
 				<div>
-					<button>
+					<NavLink
+						to="/cart"
+						className={`${
+							theme === "dark"
+								? "hover:bg-active-bg-dark"
+								: "hover:bg-nav-hov-text"
+						}  p-[1rem] rounded-[50%] relative`}
+					>
 						<FontAwesomeIcon icon={faCartShopping} />
-					</button>
+						<span
+							className={`${
+								theme === "dark"
+									? "bg-btns-bg-dark text-bg-dark"
+									: "bg-[#057AFF] text-white "
+							}  text-[1rem] px-[.5rem] rounded-[50%] absolute top-[5px] right-[3px]`}
+						>
+							0
+						</span>
+					</NavLink>
 				</div>
 			</section>
 		</nav>
---
+++ Store/src/components/header/NavLinkComponent.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react"
+import { NavLink } from "react-router-dom"
+import ThemeContext from "../../context/ThemeContext"
+
+const NavLinkComponent = ({link, label}) => {
+    const {theme} = useContext(ThemeContext)
+    
+  return (
+		<div>
+			<NavLink
+				to={link}
+				className={({ isActive }) =>
+					isActive
+                            ? `${
+                                    theme === "dark"
+                                        ? "bg-active-bg-dark"
+                                        : "bg-bg-dark"
+                            }  text-nav-hov-text px-[1rem] py-[.5rem] rounded-[7px]`
+                            : `${
+                                    theme === "dark"
+                                        ? "nav-link-dark"
+                                        : "nav-link-light"
+                            } nav-link`
+				}
+			>
+				{label}
+			</NavLink>
+		</div>
+  );
+}
+
+export default NavLinkComponent
