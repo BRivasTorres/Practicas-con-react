5de7394d2022e497cd382f4cfefd3f82843d1429
Billy Torres <btorresr75@gmail.com>
2024-04-21 07:41:00 -0700

    feat : Finish about page

--- Store/src/pages/About.jsx
+++ Store/src/pages/About.jsx
@@ -1,9 +1,45 @@
+import { useContext } from "react"
+import ThemeContext from "../context/ThemeContext"
+
 const About = () => {
+  
+  const {theme} = useContext(ThemeContext)
+  
   return (
-    <div>
-      <h2>ABOUT FADSFLDSAFDSA</h2>
-    </div>
-  )
+		<div
+			className={`${
+				theme === "dark"
+					? "bg-main-dark text-white"
+					: "bg-white text-text-light"
+			} min-h-[100dvh]`}
+		>
+			<div className="w-[80%] mx-auto pt-[3rem]">
+				<h2 className="font-bold text-[3rem] lg:text-[5rem] text-center">
+					We love{" "}
+					<span
+						className={`${
+							theme === "dark"
+								? "bg-btns-bg-dark text-text-light"
+								: "bg-btns-bg-light text-white"
+						} p-[.8rem] rounded-[20px] text-[3rem]`}
+					>
+						comfy
+					</span>
+				</h2>
+				<p className="w-[65%] mx-auto mt-[2rem] ">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Dolore, aut veniam doloribus ad fugit in ipsam dolores
+					eligendi, laborum rerum expedita doloremque, temporibus esse
+					neque consequatur iusto ut tempore repudiandae. Lorem ipsum
+					dolor sit amet, consectetur adipisicing elit. Voluptatum ex
+					temporibus illum ad enim nulla illo exercitationem odio
+					quisquam rem, delectus iste incidunt fuga excepturi pariatur
+					ullam perferendis libero. Necessitatibus! Lorem ipsum dolor
+					sit amet consectetur, adipisicing elit.
+				</p>
+			</div>
+		</div>
+  );
 }
 
 export default About
