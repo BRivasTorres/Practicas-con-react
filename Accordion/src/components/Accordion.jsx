87de96bdbe6cbc0e152122e84cfbf8c97527f454
Billy <btorresr75@gmail.com>
2024-09-10 18:58:37 -0700

    test : Test color change

--- Accordion/src/components/Accordion.jsx
+++ Accordion/src/components/Accordion.jsx
@@ -2,7 +2,7 @@ import Question from "./Question";
 
 const Accordion = () => {
 	return (
-		<div className="bg-red-700 w-[40%] max-w-[1000px] shadow-lg rounded-[10px] ">
+		<div className="bg-green-600 w-[40%] max-w-[1000px] shadow-lg rounded-[10px] ">
 			<Question />
 		</div>
 	);
--- deploy_projects.sh
+++ deploy_projects.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Lista de proyectos y sus correspondientes repositorios de GitHub
-proyectos=("accordion" "contetful cms" "birthday buddy")
-repositorios=("brivastorres/practicas-con-react/accordion" "brivastorres/practicas-con-reactconterful-cms" "brivastorres/practicas-con-react-rithday-buddy")
+proyectos=("accordion" "contentful-cms" "birthday-buddy")
+repositorios=("brivastorres/practicas-con-react/accordion" "brivastorres/practicas-con-react/contentful-cms" "brivastorres/practicas-con-react/birthday-buddy")
 
 # Ruta al directorio raíz del repositorio
 raiz_repositorio="/d/practicas de programacion/programacion fronted/practicando con react/react projects practice"
 
-# Tu token de API de Vercel
+# Tu token de API de Vercel (guárdalo de forma segura)
 vercel_token="DtbRpNq3kXz2RStUgGrKpzp2"
 
 # Iterar sobre cada proyecto y desplegarlo
@@ -32,11 +32,9 @@ for i in "${!proyectos[@]}"; do
       "type": "github",
       "repo": {
         "id": "'$repo'",
-        "repo": "'$repo'",
-        "org": "usuario", 
-        "name": "'$repo'"
-      },
-      "gitCredentialId": null
+        "org": "brivastorres", 
+        "name": "'$proyecto'"
+      }
     }'
 
   # Volver al directorio raíz
